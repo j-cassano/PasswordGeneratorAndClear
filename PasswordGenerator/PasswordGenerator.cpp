ac05219c84e648e07327ca3096fc29e28e35774c
jcassano <jcassano@live.com>
2023-11-23 15:32:03 -0800

    Add project files.

---
+++ PasswordGenerator/PasswordGenerator.cpp
@@ -0,0 +1,53 @@
+// PasswordGenerator.cpp : This file contains the 'main' function. Program execution begins and ends there.
+//
+
+#include <math.h> 
+#include <stdio.h> 
+#include <stdlib.h> 
+#include <time.h> 
+
+const int PasswordLength = 20;
+
+void ClearPassword(char password[20], int numberOfOverwrites)
+{
+    while (numberOfOverwrites--)
+    {
+        for (int i = 0; i < PasswordLength; i++)
+        {
+            password[i] = '0';
+        }
+    }
+}
+
+void PrintPassword(char password[20])
+{
+    for (int i = 0; i < PasswordLength; i++)
+    {
+        printf("%c", password[i]);
+    }
+}
+
+void GenerateRandomPassword(char* password)
+{
+    srand((time(NULL)));
+
+    char charactersUsedInGeneration[] = "0123456789abcdefghijklmnoqprstuvwyzxABCDEFGHIJKLMNOQPRSTUYWVZX!@#$^&*?-_";
+
+    for (int i = 0; i < PasswordLength; i++)
+    {
+        password[i] = charactersUsedInGeneration[rand() % sizeof(charactersUsedInGeneration)];
+    }
+}
+
+
+int main()
+{
+    char password[PasswordLength];
+    GenerateRandomPassword(password);
+    PrintPassword(password);
+    ClearPassword(password, 5);
+
+    return 0;
+}
+
+
