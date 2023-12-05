76df3c16251e658732ebbf274cd81aed5cbd30f1
jcassano <jcassano@live.com>
2023-12-04 22:41:41 -0800

    Added logic to automatically copy the password to the clipboard. Added logic to clear the clipboard after entering a key on the console.

--- PasswordGenerator/PasswordGenerator.cpp
+++ PasswordGenerator/PasswordGenerator.cpp
@@ -1,10 +1,10 @@
-// PasswordGenerator.cpp : This file contains the 'main' function. Program execution begins and ends there.
-//
-
 #include <math.h> 
 #include <stdio.h> 
 #include <stdlib.h> 
 #include <time.h> 
+#include <windows.h> 
+#include <string>
+#include "PasswordGenerator.h"
 
 const int PasswordLength = 20;
 
@@ -25,6 +25,7 @@ void PrintPassword(char password[PasswordLength])
     {
         printf("%c", password[i]);
     }
+    printf("\n");
 }
 
 void GenerateRandomPassword(char* password)
@@ -39,13 +40,65 @@ void GenerateRandomPassword(char* password)
     }
 }
 
+void GenerateEmptyPassword(char* password)
+{
+    for (int i = 0; i < PasswordLength; i++)
+    {
+        password[i] = '0';
+    }
+}
+
+void CopyPasswordToClipboard(char password[PasswordLength])
+{
+    HWND hwnd = GetConsoleWindow();
+    OpenClipboard(hwnd);
+    EmptyClipboard();
+    HGLOBAL hg = GlobalAlloc(GMEM_MOVEABLE, PasswordLength + 1);
+    memcpy(GlobalLock(hg), password, PasswordLength + 1);
+    GlobalUnlock(hg);
+    SetClipboardData(CF_TEXT, hg);
+    CloseClipboard();
+    ClearHGlobal(hg);
+}
+
+void ClearHGlobal(const HGLOBAL& hg)
+{
+    char clearedPassword[PasswordLength];
+    GenerateEmptyPassword(clearedPassword);
+    memcpy(GlobalLock(hg), clearedPassword, PasswordLength + 1);
+    GlobalUnlock(hg);
+    GlobalFree(hg);
+}
+
+void PrintMessageAndWaitToClear()
+{
+    printf("The password is now in the clipboard. \n");
+    printf("After pasting the password press enter to clear the password from memory including the clipboard.\n");
+    char c = getchar();
+}
+
+void ClearClipBoard()
+{
+    HWND hwnd = GetConsoleWindow();
+    OpenClipboard(hwnd);
+    EmptyClipboard();
+    CloseClipboard();
+}
+
+void PrintClearedMessage()
+{
+    printf("The password has been cleared. \n");
+}
 
 int main()
 {
     char password[PasswordLength];
     GenerateRandomPassword(password);
-    PrintPassword(password);
+    CopyPasswordToClipboard(password);
+    PrintMessageAndWaitToClear();
+    ClearClipBoard();
     ClearPassword(password, 5);
+    PrintClearedMessage();
 
     return 0;
 }
--- PasswordGenerator/PasswordGenerator.h
+++ PasswordGenerator/PasswordGenerator.h
@@ -1,3 +1,3 @@
 #pragma once
 
-
+void ClearHGlobal(const HGLOBAL& hg);
